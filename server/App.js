08aee0c64a9f8f87bea4307bd1876d35077c9945
Q7 <sihle@thedigitalacademy.co.za>
2021-05-10 16:01:15 +0200

    Database connection working

--- server/App.js
+++ server/App.js
@@ -1,34 +1,35 @@
 // npm install express
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const router = require("./routes/routes");
 
-
-//Init 
+//Init
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // use middlewares
 app.use(cors());
-app.use(bodyParser.json({limit: '50mb',extended: true}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 
-
 //Connection to database
-const CONNECTION_URL = 'mongodb+srv://Fastya:@Fastya89.com@cluster0.jcthv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL =
+  "mongodb+srv://Sihle:abXD5pBFgwj04T9k@cluster0.jcthv.mongodb.net/Fastya?retryWrites=true&w=majority";
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true}).
-then(()=>app.listen(PORT,()=>console.log('Server running on port: ${PORT}')))
-.catch((error)=>console.log(error.message));
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    console.log(`Database is running`)
+  )
+  .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify',false);
+mongoose.set("useFindAndModify", false);
 
 //Using routes
 app.use(router);
-
 
 //server listen
 app.listen(PORT, () => console.log(`Server up on Port: ${PORT}`));
